4d1d7e718cee7f9edad8a04bb501222d19c0f41b
ZeCroque <zobilezeub@gmail.com>
2025-05-16 23:33:41 +0200

    Move log function at the bottom and add clear split Papyrus/SKSE

--- src/main.cpp
+++ src/main.cpp
@@ -7,36 +7,10 @@
 #include "Engine/Package.h"
 #include "Engine/Quest.h"
 
-void InitializeLog()
-{
-#ifndef NDEBUG
-	auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
-#else
-	auto path = SKSE::log::log_directory();
-	if (!path) 
-	{
-		util::report_and_fail("Failed to find standard logging directory"sv);
-	}
-
-	*path /= fmt::format("{}.log"sv, Plugin::NAME);
-	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
-#endif
-
-#ifndef NDEBUG
-	constexpr auto level = spdlog::level::trace;
-#else
-	constexpr auto level = spdlog::level::info;
-#endif
-
-	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
-	log->set_level(level);
-	log->flush_on(level);
-
-	spdlog::set_default_logger(std::move(log));
-	spdlog::set_pattern("%g(%#): [%^%l%$] %v"s);
-}
-
 RE::TESQuest* referenceQuest = nullptr;
+
+// # Papyrus
+// =======================
 RE::TESQuest* generatedQuest = nullptr;
 RE::TESQuest* selectedQuest = nullptr;
 RE::TESObjectREFR* targetForm = nullptr;
@@ -357,6 +331,38 @@ bool RegisterFunctions(RE::BSScript::IVirtualMachine* inScriptMachine)
 	inScriptMachine->RegisterFunction("StartSelectedQuest", "SQGLib", StartSelectedQuest);
 	inScriptMachine->RegisterFunction("DraftDebugFunction", "SQGLib", DraftDebugFunction);
 	return true;
+}
+
+// # SKSE
+// =======================
+
+void InitializeLog()
+{
+#ifndef NDEBUG
+	auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
+#else
+	auto path = SKSE::log::log_directory();
+	if (!path) 
+	{
+		util::report_and_fail("Failed to find standard logging directory"sv);
+	}
+
+	*path /= fmt::format("{}.log"sv, Plugin::NAME);
+	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+#endif
+
+#ifndef NDEBUG
+	constexpr auto level = spdlog::level::trace;
+#else
+	constexpr auto level = spdlog::level::info;
+#endif
+
+	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
+	log->set_level(level);
+	log->flush_on(level);
+
+	spdlog::set_default_logger(std::move(log));
+	spdlog::set_pattern("%g(%#): [%^%l%$] %v"s);
 }
 
 // ReSharper disable once CppInconsistentNaming
