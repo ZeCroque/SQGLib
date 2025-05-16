3d26327d58755896fa21fc9ae51f2328c2ddb76f
ZeCroque <zobilezeub@gmail.com>
2025-05-16 18:37:41 +0200

    Namespaced utils classes

--- includes/SQG/API/DialogUtils.h
+++ includes/SQG/API/DialogUtils.h
@@ -1,27 +1,30 @@
 #pragma once
 
-constexpr int SUB_TOPIC_COUNT = 4;
-
-struct DialogTopicData
+namespace SQG
 {
-	struct AnswerData
+	constexpr int SUB_TOPIC_COUNT = 4;
+
+	struct DialogTopicData
 	{
-		DialogTopicData* parentEntry; 
-		RE::BSFixedString answer;
-		RE::BSFixedString promptOverride;
-		std::vector<RE::TESConditionItem*> conditions;
-		int targetStage;
-		int fragmentId;
-		bool alreadySaid { false };
+		struct AnswerData
+		{
+			DialogTopicData* parentEntry; 
+			RE::BSFixedString answer;
+			RE::BSFixedString promptOverride;
+			std::vector<RE::TESConditionItem*> conditions;
+			int targetStage;
+			int fragmentId;
+			bool alreadySaid { false };
+		};
+
+		void AddAnswer(const RE::BSString& inTopicInfoText, const RE::BSString& inTopicOverrideText, const std::vector<RE::TESConditionItem*>& inConditions, int inTargetStage = -1, int inFragmentId = -1);
+
+		RE::TESQuest* owningQuest;
+		RE::BSFixedString prompt;	// in CK : Topic text max 80/TopicInfo text max 150 -> TODO test if hardcap
+		std::vector<AnswerData> answers;
+		std::vector<std::unique_ptr<DialogTopicData>> childEntries;
 	};
+	extern std::map<RE::FormID, DialogTopicData> dialogTopicsData;
 
-	void AddAnswer(const RE::BSString& inTopicInfoText, const RE::BSString& inTopicOverrideText, const std::vector<RE::TESConditionItem*>& inConditions, int inTargetStage = -1, int inFragmentId = -1);
-
-	RE::TESQuest* owningQuest;
-	RE::BSFixedString prompt;	// in CK : Topic text max 80/TopicInfo text max 150 -> TODO test if hardcap
-	std::vector<AnswerData> answers;
-	std::vector<std::unique_ptr<DialogTopicData>> childEntries;
-};
-extern std::map<RE::FormID, DialogTopicData> dialogTopicsData;
-
-DialogTopicData* AddDialogTopic(RE::TESQuest* inQuest, const RE::TESObjectREFR* inSpeaker, const std::string& inPrompt = "", DialogTopicData* inParentTopic = nullptr);
+	DialogTopicData* AddDialogTopic(RE::TESQuest* inQuest, const RE::TESObjectREFR* inSpeaker, const std::string& inPrompt = "", DialogTopicData* inParentTopic = nullptr);
+}
