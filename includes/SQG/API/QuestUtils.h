aea889aa162dce3ae080c18a6836636a969cf1c1
ZeCroque <zobilezeub@gmail.com>
2025-05-16 23:24:05 +0200

    Moved package logic in its own file

--- includes/SQG/API/QuestUtils.h
+++ includes/SQG/API/QuestUtils.h
@@ -28,6 +28,13 @@ namespace SQG
 		};
 		std::vector<std::unique_ptr<Objective>> objectives;
 
+		struct AliasPackageData
+		{
+			RE::TESPackage* package;
+			std::string fragmentScriptName;
+		};
+		std::map<RE::FormID, std::vector<AliasPackageData>> aliasesPackagesData;
+
 		RE::BSScript::Object* script = nullptr;
 		std::map<uint16_t, int> stagesToFragmentIndex;
 	};
@@ -38,4 +45,5 @@ namespace SQG
 	void AddQuestStage(RE::TESQuest* inQuest, std::uint16_t inIndex, QuestStageType inQuestStageType = QuestStageType::Default, const std::string& inLogEntry = "", int inScriptFragmentIndex = -1);
 	void AddObjective(RE::TESQuest* inQuest, std::uint16_t inIndex, const std::string& inText, const std::vector<uint8_t>& inQuestTargetsAliasIndexes = std::vector<uint8_t>());
 	void AddRefAlias(RE::TESQuest* inQuest, std::uint16_t inIndex, const std::string& inName, RE::TESObjectREFR* inRef); //TODO do other aliases (not forced, location...)
+	void AddAliasPackage(const RE::TESQuest* inQuest, const RE::TESObjectREFR* inRef, RE::TESPackage* inPackage, const std::string& inFragmentScriptName);
 }
