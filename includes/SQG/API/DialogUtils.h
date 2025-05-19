a8d9a11191b64bc56ae7855d158ada95e7daff6c
ZeCroque <zobilezeub@gmail.com>
2025-05-19 17:58:50 +0200

    Changed PackageUtils to be more inline with other utils Removed condition handling from PackageUtils Changed vectors by list in many places because direct access was never a thing Reserved topic vectors since size if known

--- includes/SQG/API/DialogUtils.h
+++ includes/SQG/API/DialogUtils.h
@@ -11,17 +11,17 @@ namespace SQG
 			DialogTopicData* parentEntry; 
 			RE::BSFixedString answer;
 			RE::BSFixedString promptOverride;
-			std::vector<RE::TESConditionItem*> conditions;
+			std::list<RE::TESConditionItem*> conditions;
 			int targetStage;
 			int fragmentId;
 			bool alreadySaid { false };
 		};
 
-		void AddAnswer(const RE::BSString& inTopicInfoText, const RE::BSString& inTopicOverrideText, const std::vector<RE::TESConditionItem*>& inConditions, int inTargetStage = -1, int inFragmentId = -1);
+		void AddAnswer(const RE::BSString& inTopicInfoText, const RE::BSString& inTopicOverrideText, const std::list<RE::TESConditionItem*>& inConditions, int inTargetStage = -1, int inFragmentId = -1);
 
 		RE::TESQuest* owningQuest;
 		RE::BSFixedString prompt;	// in CK : Topic text max 80/TopicInfo text max 150 -> TODO test if hardcap
-		std::vector<AnswerData> answers;
+		std::list<AnswerData> answers;
 		std::vector<std::unique_ptr<DialogTopicData>> childEntries;
 	};
 	extern std::map<RE::FormID, DialogTopicData> dialogTopicsData;
@@ -30,7 +30,7 @@ namespace SQG
 
 	DialogTopicData* AddDialogTopic(RE::TESQuest* inQuest, const RE::TESObjectREFR* inSpeaker, const std::string& inPrompt = "", DialogTopicData* inParentTopic = nullptr);
 
-	void AddForceGreet(RE::TESQuest* inQuest, const RE::TESObjectREFR* inSpeaker, const std::string& inForceGreet, const std::list<struct PackageConditionDescriptor>& inConditions);
+	void AddForceGreet(RE::TESQuest* inQuest, const RE::TESObjectREFR* inSpeaker, const std::string& inForceGreet, const std::list<RE::TESConditionItem*>& inConditions);
 
 	void AddHelloTopic(const RE::TESObjectREFR* inSpeaker, const std::string& inHello);
 }
